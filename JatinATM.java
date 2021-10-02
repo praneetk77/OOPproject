4c91c26e98e4cf8d27b1878ec1538d1f651b03ca
praneetk77 <praneetkarna77@gmail.com>
2021-10-02 22:40:48 +0530

    Corrected same name files problem

---
+++ JatinATM.java
@@ -0,0 +1,66 @@
+package OOPproject;
+
+import java.util.*;
+class jatin_ATM{
+	User u;
+	
+}
+class jatin_Acc{
+int accnumber;
+int balance;
+boolean checkacc(int acc) {
+	if(acc==this.accnumber) {
+		return true;
+	}else {
+		return false;
+	}
+}
+void showBalance() {
+	System.out.println(this.balance);
+}
+}
+class JatinCard{
+	int pin;
+	JatinCard(int pin){
+		this.pin=pin;
+	}
+	boolean checkpin(int pin) {
+		if(pin==this.pin) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	void show() {
+		System.out.println(this.pin);
+	}
+}
+class JatinUser{
+	String name;
+	int a;
+	int p;
+	 JatinUser(String name,int a,int p){
+		this.name=name;
+		this.a=a;
+		this.p=p;
+	}
+	void show() {
+		System.out.println(this.name+" "+this.a+" "+this.p+" ");
+	}
+}
+public class JatinATM {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter the details of a new user:");
+		String a=obj.nextLine();
+		int b=obj.nextInt();
+		int c=obj.nextInt();
+		JatinUser u=new JatinUser(a,b,c);
+		Card c1=new Card(c);
+		System.out.println("Enter the details of an existing user: ");
+		
+//		u.show();
+	}
+
+}
--- atm.java
+++ atm.java
@@ -1,64 +1,86 @@
-import java.util.*;
-class ATM{
-	User u;
+package OOPproject;
+
+import java.util.Scanner;
+
+
+public class ATM {
+	static Scanner sc = new Scanner(System.in);
+	User user;
 	
-}
-class Acc{
-int accnumber;
-int balance;
-boolean checkacc(int acc) {
-	if(acc==this.accnumber) {
-		return true;
-	}else {
-		return false;
+	ATM(User user){
+		this.user = user;
 	}
-}
-void showBalance() {
-	System.out.println(this.balance);
-}
-}
-class Card{
-	int pin;
-	Card(int pin){
-		this.pin=pin;
+	
+	public int enterPin() {
+		System.out.println("\nPlease enter your pin :-\n");
+		return sc.nextInt();
 	}
-	boolean checkpin(int pin) {
-		if(pin==this.pin) {
-			return true;
-		}else {
+	
+	public boolean selectTransaction(int choice) {
+		if(choice==1) {
+			this.user.showBalance();
+		}else if(choice==2) {
+			int amount = enterAmount();
+			this.user.deposit(amount);
+		}else if(choice==3) {
+			int amount = enterAmount();
+			this.user.withdraw(amount);
+		}else if(choice==4) {
+			this.displayExitMessage();
 			return false;
 		}
+		return true;
 	}
-	void show() {
-		System.out.println(this.pin);
+	
+	public int enterAmount() {
+		System.out.println("\nPlease enter your amount :-\n");
+		return sc.nextInt();
 	}
-}
-class User{
-	String name;
-	int a;
-	int p;
-	 User(String name,int a,int p){
-		this.name=name;
-		this.a=a;
-		this.p=p;
-	}
-	void show() {
-		System.out.println(this.name+" "+this.a+" "+this.p+" ");
-	}
-}
-public class atm {
-
-	public static void main(String[] args) {
-		Scanner obj=new Scanner(System.in);
-		System.out.println("Enter the details of a new user:");
-		String a=obj.nextLine();
-		int b=obj.nextInt();
-		int c=obj.nextInt();
-		User u=new User(a,b,c);
-		Card c1=new Card(c);
-		System.out.println("Enter the details of an existing user: ");
+	
+	public void displayWelcomeMessage() {
 		
-//		u.show();
+		System.out.println("\nHello. Welcome to OOP Bank.");
+	}
+	
+	public void displayChoiceMessage() {
+		System.out.println("\nEnter :-\n1 --> To show balance.\n2 --> To deposit money.\n3 --> To withdraw money.\n4 --> To exit.\n");
+	}
+	
+	public void displayPinErrorMessage() {
+		System.out.println("\nYou have entered the wrong pin. Please try again later ");
+	}
+	
+	public void displayErrorMessage() {
+		System.out.println("\nError. Please try again later.");
+	}
+	
+	public void displayExitMessage() {
+		System.out.println("\nThank you. See you soon.");
+	}
+	
+	public static void main(String[] args) {
+		Card card1 = new Card(1234);
+		Account account1 = new Account(5000);
+		User user1 = new User(card1, account1);
+		
+		ATM atm = new ATM(user1);
+		atm.displayWelcomeMessage();
+		
+		int enteredPin = atm.enterPin();
+		if(atm.user.card.checkPin(enteredPin)) {
+			
+			while(true) {
+				atm.displayChoiceMessage();
+				int choice = sc.nextInt();
+				if(!atm.selectTransaction(choice)) break;
+			}
+			
+			
+			
+		}else {
+			atm.displayPinErrorMessage();
+		}
+		
 	}
 
 }
